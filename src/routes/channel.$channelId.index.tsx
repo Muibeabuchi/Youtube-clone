2455bdb2bb2f3680d4dc3c47c805b9f05ccec71e
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2025-06-20 02:21:55 +0100

    FEAT: Add prefetching of channel videos data in the channels index page

--- src/routes/channel.$channelId.index.tsx
+++ src/routes/channel.$channelId.index.tsx
@@ -1,8 +1,29 @@
+import { singleChannelQueryOptions } from "@/utils/channel";
 import { channelSectionInfoOptions } from "@/utils/channel-sections";
+import { fetchChannelsUploadedVideosPlaylistItemOptions } from "@/utils/playlist-items";
+import { channelPlaylistVideoOptions } from "@/utils/videos";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/channel/$channelId/")({
   loader: async ({ context, params }) => {
+    // Prefetch the data for the Videos page
+    const channelId = params.channelId;
+    const channelInfo = await context.queryClient.ensureQueryData(
+      singleChannelQueryOptions(channelId)
+    );
+
+    const channelVideoPlaylistId =
+      channelInfo.items?.[0].contentDetails.relatedPlaylists.uploads;
+
+    const videoIds = await context.queryClient.ensureQueryData(
+      fetchChannelsUploadedVideosPlaylistItemOptions(channelVideoPlaylistId)
+    );
+    const videoIdsString = videoIds.map((v) => v.videoId).join(",");
+
+    context.queryClient.prefetchQuery(
+      channelPlaylistVideoOptions(videoIdsString, params.channelId)
+    );
+
     const channelSectionInfo = await context.queryClient.ensureQueryData(
       channelSectionInfoOptions(params.channelId)
     );
